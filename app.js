8e2a2d2c980122082f1d54551d6e2d9028bbe1ac
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-02-01 09:21:57 +0600

    smooth scroll

--- app.js
+++ app.js
@@ -4,10 +4,62 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links")
 
+//** Self-Explanation **//
+
+//If we modify css using js it gets added as inline css which has more specificity than external css. To override the height in media queries thats why !important is added.
+// toggle method doesn't require the use of important. But the height needs to be added custom everytime a new nav link is added unlike getBoundingClientRect() method
+
+navToggle.addEventListener("click", function () {
+    // linksContainer.classList.toggle("show-links")
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0;
+    }
+})
+
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link")
 // ********** fixed navbar ************
-
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+    if (scrollHeight > 500) {
+        topLink.classList.add("show-link");
+    } else {
+        topLink.classList.remove("show-link")
+    }
+})
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        //prevent default behavior
+        e.preventDefault();
+        //navigate to speicic spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id)
+        let position = element.offsetTop;
+        console.log(position)
+    })
+})
+
+
